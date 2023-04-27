e88d1214f084c0beccb249047062c557a3ff59f1
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-27 12:07:53 -0300

    Exercicios 4 modicado

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -3,18 +3,25 @@ public class Exercicio4 {
         int n=10;
         System.out.println("USANDO FOR");
         for (int i = 1; i < n; i++) {
-            System.out.println(i);
+            if (i%2==0){
+                System.out.println(i);
+            }
+
         }
         System.out.println("USANDO WHILE");
         int cont=1;
         while (cont<n){
-            System.out.println(cont);
+            if (cont%2==0){
+                System.out.println(cont);
+            }
             cont++;
         }
         System.out.println("USANDO DO WHILE");
         int contDoWhile = 1;
         do {
-            System.out.println(contDoWhile);
+            if (contDoWhile%2==0){
+                System.out.println(contDoWhile);
+            }
             contDoWhile++;
         }while (contDoWhile<n);
 
